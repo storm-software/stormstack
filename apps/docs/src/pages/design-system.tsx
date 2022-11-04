c2110608dd7cccc197ab220cd15c29ee1c763157
sullivanp <patrick.sullivan@broadridge.com>
2022-11-04 08:37:42 -0400

    docs: Updated doc website styling

--- apps/docs/src/pages/design-system.tsx
+++ apps/docs/src/pages/design-system.tsx
@@ -1,8 +1,8 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import clsx from "clsx";
 import React from "react";
 import DesignTokensContainer from "../components/DesignTokens/design-tokens-container.mdx";
+import Hero from "../components/Hero/Hero";
 import styles from "./design-system.module.css";
 
 export default function DesignSystem(): JSX.Element {
@@ -11,12 +11,7 @@ export default function DesignSystem(): JSX.Element {
     <Layout
       title={siteConfig.title}
       description="Development and Application Documentation">
-      <header className={clsx("hero hero--primary", styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title title">Open System</h1>
-          <h2 className="text_white-glow subtitle">Design System</h2>
-        </div>
-      </header>
+        <Hero title="Design System" />
       <main>
         <div className="container">
           <div className={styles.designTokensContainer}>
--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,47 +1,10 @@
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import clsx from "clsx";
 import React from "react";
+import Hero from "../components/Hero/Hero";
 import HomepageFeatures from "../components/HomepageFeatures/HomepageFeatures";
-import styles from "./index.module.css";
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle text_white-glow">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Learn More 🎓
-          </Link>
-
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            API Reference 📓
-          </Link>
-
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Repository Tools 🔧
-          </Link>
-
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            OpenAPI Specs 🖥
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
@@ -49,7 +12,28 @@ export default function Home(): JSX.Element {
     <Layout
       title={siteConfig.title}
       description="Development and Application Documentation">
-      <HomepageHeader />
+       <Hero title={siteConfig.tagline}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/getting-started/installation">
+            Learn More 🎓
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            OpenAPI Specs 🖥
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Repository Tools 🔧
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/design-system">
+            Design System 📓
+          </Link>
+        </Hero>
       <main>
         <HomepageFeatures />
       </main>
