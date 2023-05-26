8bc827b67520268de30322e9dfed8e6375d11b81
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2023-05-26 01:35:13 -0400

    chore(repo): Remove unused web app projects

--- apps/web/contact/app/error.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ErrorBoundaryProps } from "@open-system/shared-ui-components";
-import { ErrorBoundary } from "@open-system/shared-ui-feature-layout/error-boundary";
-
-export default function Error(props: ErrorBoundaryProps) {
-  return <ErrorBoundary {...props} />;
-}
--- apps/web/contact/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="space-y-8">
-      <h1 className="text-xl font-medium text-gray-300">
-        Were in a remote component
-      </h1>
-    </div>
-  );
-}
