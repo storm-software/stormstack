177674daec52ebf039385aec1c5c94a809432a75
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2023-06-26 10:26:15 -0400

    refactor(web-shell): Load fonts from static directory

--- apps/web/shell/app/dev/(introduction)/monitor.tsx
+++ apps/web/shell/app/dev/(introduction)/monitor.tsx
@@ -4,7 +4,7 @@ import { MinusIcon, PlusIcon, PowerIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import dynamic from "next/dynamic";
 import { useCallback, useEffect, useState } from "react";
-import DellLogo from "../../../../../../assets/images/dell-logo.svg";
+import DellLogo from "../../../public/static/images/dell-logo.svg";
 
 const Screen = dynamic(() => import("./screen"), {
   loading: () => (
@@ -43,13 +43,13 @@ export default function Monitor() {
     <div className="grid-col-1 grid justify-items-center">
       <div className="relative z-20 h-[35rem] w-full rounded-lg border border-white bg-slate-500 p-5 pb-8 shadow-[7px_7px_7px_0px_rgba(0,0,0,0.3)] md:h-[30rem]">
         {isPowered ? (
-          <div className="relative h-full w-full rounded-sm border-t-4 border-r-4 border-slate-700 bg-bg-windows bg-cover bg-center bg-no-repeat">
+          <div className="relative h-full w-full rounded-sm border-r-4 border-t-4 border-slate-700 bg-bg-windows bg-cover bg-center bg-no-repeat">
             <Screen />
           </div>
         ) : (
-          <div className="h-full w-full border-t-4 border-r-4 border-slate-700 bg-black" />
+          <div className="h-full w-full border-r-4 border-t-4 border-slate-700 bg-black" />
         )}
-        <div className="absolute -bottom-[5rem] left-2 z-50 flex h-24 w-28 -rotate-[4deg] flex-col justify-center gap-0.5 bg-yellow-200 text-center before:absolute before:-bottom-[9px] before:right-[11px] before:z-40 before:w-0 before:translate-y-1/2 before:translate-x-1/2 before:border-t-[20px] before:border-r-[20px] before:border-r-transparent before:border-t-yellow-100 before:bg-transparent before:shadow-[-2px_-2px_2px_0px_rgba(0,0,0,0.3)] before:content-[''] after:absolute after:-bottom-[9px] after:right-1/2 after:z-30 after:w-full after:translate-y-1/2 after:translate-x-1/2 after:border-t-[20px] after:border-r-[20px] after:border-r-transparent after:border-t-yellow-200 after:bg-transparent after:content-[''] sm:w-32 md:left-10">
+        <div className="absolute -bottom-[5rem] left-2 z-50 flex h-24 w-28 -rotate-[4deg] flex-col justify-center gap-0.5 bg-yellow-200 text-center before:absolute before:-bottom-[9px] before:right-[11px] before:z-40 before:w-0 before:translate-x-1/2 before:translate-y-1/2 before:border-r-[20px] before:border-t-[20px] before:border-r-transparent before:border-t-yellow-100 before:bg-transparent before:shadow-[-2px_-2px_2px_0px_rgba(0,0,0,0.3)] before:content-[''] after:absolute after:-bottom-[9px] after:right-1/2 after:z-30 after:w-full after:translate-x-1/2 after:translate-y-1/2 after:border-r-[20px] after:border-t-[20px] after:border-r-transparent after:border-t-yellow-200 after:bg-transparent after:content-[''] sm:w-32 md:left-10">
           <label className="text-xl font-label-2 text-slate-900">
             Who is Pat Sullivan?
           </label>
@@ -80,7 +80,7 @@ export default function Monitor() {
         </div>
       </div>
       <div className="h-16 w-64 justify-self-center border-l-4 border-r border-l-gray-800 border-r-gray-300 bg-gradient-to-t from-gray-400 to-gray-800 bg-bottom bg-no-repeat"></div>
-      <div className="h-2.5 w-96 justify-self-center rounded border-r border-b border-r-white border-b-white bg-slate-600"></div>
+      <div className="h-2.5 w-96 justify-self-center rounded border-b border-r border-b-white border-r-white bg-slate-600"></div>
     </div>
   );
 }
--- apps/web/shell/styles/fonts.ts
+++ apps/web/shell/styles/fonts.ts
@@ -3,32 +3,32 @@ import localFont from "@next/font/local";
 
 export const melody = localFont({
   variable: "--font-melody",
-  src: "../../../../assets/fonts/BLMelody-Bold.woff2",
+  src: "/static/fonts/BLMelody-Bold.woff2",
 });
 
 export const satoshi = localFont({
   variable: "--font-satoshi",
-  src: "../../../../assets/fonts/Satoshi-Bold.otf",
+  src: "/static/fonts/Satoshi-Bold.otf",
 });
 
 export const antiqueOlive = localFont({
   variable: "--font-antique-olive",
-  src: "../../../../assets/fonts/Antique-Olive-Black.ttf",
+  src: "/static/fonts/Antique-Olive-Black.ttf",
 });
 
 export const permanentMarker = localFont({
   variable: "--font-permanent-marker",
-  src: "../../../../assets/fonts/PermanentMarker-Regular.ttf",
+  src: "/static/fonts/PermanentMarker-Regular.ttf",
 });
 
 export const monaSans = localFont({
   variable: "--font-mona-sans",
-  src: "../../../../assets/fonts/Mona-Sans-Light.woff2",
+  src: "/static/fonts/Mona-Sans-Light.woff2",
 });
 
 export const monaSansExtraBold = localFont({
   variable: "--font-mona-sans-extrabold",
-  src: "../../../../assets/fonts/Mona-Sans-ExtraBold.woff2",
+  src: "/static/fonts/Mona-Sans-ExtraBold.woff2",
 });
 
 /*export const anybody = Anybody({
