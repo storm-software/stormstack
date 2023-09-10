7c207e0ca4845f336ca51de72ac11f262c35cca6
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2023-09-09 22:07:31 -0400

    fix(tools-generators-typescript): Resolved build issues with @swc/core library

--- libs/core/typescript/server/cloudflare/src/utilities/wrangler.ts
+++ libs/core/typescript/server/cloudflare/src/utilities/wrangler.ts
@@ -5,6 +5,7 @@ import {
   workspaceRoot
 } from "@nx/devkit";
 import { executeAsync } from "@open-system/core-server-utilities/execute";
+import { ConfigurationError } from "@open-system/core-shared-utilities";
 import { isSet } from "@open-system/core-shared-utilities/common/type-checks";
 import { ConsoleLogger } from "@open-system/core-shared-utilities/logging";
 import { FsTree } from "nx/src/generators/tree";
@@ -17,31 +18,28 @@ export async function runWranglerCommand(
 ) {
   const { projectName } = context;
 
+  if (!projectName) {
+    ConsoleLogger.error("No project name was provided in context object.");
+    throw new ConfigurationError("projectName");
+  }
+
   const tree = new FsTree(process.cwd(), false);
   const projectConfiguration = readProjectConfiguration(tree, projectName);
+  const configuration = projectConfiguration?.targets?.["build"]?.options ?? {};
 
   const wranglerOptions = [];
   if (command === "deploy") {
     wranglerOptions.push(
-      Path.join(
-        workspaceRoot,
-        projectConfiguration.targets["build"].options.outputPath,
-        "worker.mjs"
-      )
+      Path.join(workspaceRoot, configuration.outputPath, "worker.mjs")
     );
   } else if (command === "dev") {
-    wranglerOptions.push(
-      Path.join(
-        workspaceRoot,
-        projectConfiguration.targets["build"].options.main
-      )
-    );
+    wranglerOptions.push(Path.join(workspaceRoot, configuration.main));
   }
 
   const nextCommand = `NO_D1_WARNING=true npx wrangler ${command} ${wranglerOptions.join(
     " "
   )} ${Object.entries(options).reduce(
-    (ret: string, [key, value]: [string, string]) => {
+    (ret: string, [key, value]: [string, string | number | boolean]) => {
       if (isSet(key) && isSet(value)) {
         ret += ` --${key}=${value} `;
       }
